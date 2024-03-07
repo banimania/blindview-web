6b2419e8ebecd9dd6b10777e5afea3b98104499f
banimania <79039680+banimania@users.noreply.github.com>
2024-03-07 10:51:47 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -13,27 +13,27 @@ function App() {
       <div id="videodiv">
         <div id="text">
           <h2 class="whatisit">¿Qué es BlindView?</h2>
-          <p class="blindview">BlindView es una aplicación Android específicamente diseñada para asistir y aumentar la independencia de las personas invidentes.<br/><br/> La aplicación utiliza la cámara y el sensor GPS del dispositivo con el fin de orientar al usuario en su entorno físico de forma precisa y en tiempo real, mediante tecnología texto-a-voz e inteligencia artificial, la cual puede ser controlada a través de una sencilla y accesible interfaz de audio compuesta por varios modos y comandos.</p>
+          <p class="blindview">BlindView es una aplicación Android específicamente diseñada para asistir y mejorar la independencia de las personas invidentes.<br/><br/> La aplicación utiliza la cámara y el sensor GPS del dispositivo telefónico con el fin de orientar al usuario en su entorno físico de forma precisa y en tiempo real, mediante inteligencia artificial. Esta puede ser controlada a través de una sencilla e intuitiva de usuario compuesta por varios modos y comandos de voz.</p>
         </div>
         <video src="ad.mp4" id="video" controls></video>
       </div>
 
       <h2 class="benefits">Ventajas</h2>
       <div id="wrapper">
-        <Feature name="Accesibilidad" description="Nuestra sencilla interfaz de audio integrada nos permite compartir los grandes avances y beneficios de la inteligencia artificial con todo el mundo." icon="./icons/accesibilidad.png"/>
-        <Feature name="Uso gratuito" description="A diferencia de muchas otras soluciones, esta aplicación es de uso gratuito y de código abierto. Además, es posible administrar las claves utilizadas." icon="./icons/dollar.png"/>
-        <Feature name="Integración" description="Nuestra interfaz puede hacer uso de ciertas funciones como la ubicación o el giroscopio, con el objetivo de orientar al usuario en el espacio que lo rodea." icon="./icons/gps.png"/>
-        <Feature name="Modos" description="La interfaz proporciona una gran variedad de modos con diferentes funcionalidades y activables mediante palabras clave que son accesibles mediante un menú." icon="./icons/settings.png"/>
+        <Feature name="Accesibilidad" description="Nuestra sencilla interfaz integrada nos permite compartir los grandes avances y beneficios de la inteligencia artificial con las personas más dependientes." icon="./icons/accesibilidad.png"/>
+        <Feature name="Bajo coste" description="A diferencia de muchas otras soluciones, esta aplicación es de uso prácticamente gratuito y de código abierto. Además, es posible administrar las claves utilizadas." icon="./icons/dollar.png"/>
+        <Feature name="Integración" description="Nuestra interfaz puede hacer uso de ciertas funciones como la ubicación, el giroscopio o la cámara del dispositivo, con el objetivo de orientar al usuario en el espacio que lo rodea." icon="./icons/gps.png"/>
+        <Feature name="La aplicación" description="La aplicación proporciona una gran variedad de modos con diferentes funcionalidades y nos permite acceder a un sin fin de opciones mediante palabras clave seleccionables vía voz." icon="./icons/settings.png"/>
       </div>
 
       <h2 class="modes">Modos</h2>
       <div id="wrapper-modes">
-        <Mode name="Orientación" keyword="orientación" description="Cuando este modo está activado, el dispositivo realiza fotografías cada 20 segundos y describe el ambiente visto por la cámara, advirtiendo al usuario de cualquier peligro en el camino."/>
-        <Mode name="Indicaciones" keyword="indicaciones" description="Una vez se activa este modo, la interfaz preguntará al usuario a dónde desea ir. Para el correcto funcionamiento de este modo, es necesario activar la ubicación del dispositivo. La interfaz le recordará las indicaciones desde su ubicación actual cuando el usuario diga 'actualizar ruta'."/>
-        <Mode name="Semáforos" keyword="semáforos" description="Este comando toma una fotografía a través de la cámara e identifica todos los semáforos presentes en la imagen captada, informando al usuario del estado de los mismos y de cualquier obstáculo."/>
-        <Mode name="Pregunta" example="pregunta ¿Cuánto es 10x3?" keyword="pregunta" description="En el caso de que el usuario quiera acceder a la inteligencia artificial de texto, podrá realizar preguntas a través de este comando, obteniendo un tiempo de respuesta mucho mejor que si enviase la imagen."/>
-        <Mode name="Modo libre" example="¿De qué color es el suelo?" keyword="Ninguna" description="Si ninguna funcionalidad se adecúa a las necesidades del usuario, será posible utilizar el modo libre para mandar la imagen vista por la cámara a la inteligencia artificial, acompañada por las indicaciones del usuario."/>
-        <Mode name="Menú" keyword="menú" description="Lee los modos y comandos disponibles."/>
+        <Mode name="Orientación" keyword="orientación" description="Cuando este modo está activado, el dispositivo realiza capturas periódicamente y describe el ambiente percibido por la cámara, advirtiendo al usuario de cualquier peligro en el camino."/>
+        <Mode name="Indicaciones" keyword="indicaciones" description="Una vez se activa este modo, la interfaz preguntará al usuario a dónde desea ir. Para el correcto funcionamiento de este modo, es necesario activar la ubicación del dispositivo. La aplicación refrescará las indicaciones desde su ubicación actual cuando el usuario diga 'actualizar ruta'."/>
+        <Mode name="Semáforos" keyword="semáforos" description="Esta opción nos informa de nuestro entorno e identifica todos los semáforos presentes en el ambiente percibido, informando al usuario del estado de los mismos y de cualquier obstáculo relevante."/>
+        <Mode name="Pregunta" example="pregunta ¿Cuánto es 10x3?" keyword="pregunta" description="En el caso de que el usuario quiera acceder a la inteligencia artificial, podrá realizar preguntas a través de esta opción, obteniendo un tiempo de respuesta mucho mejor que si enviase la imagen."/>
+        <Mode name="Modo libre" example="¿De qué color es el suelo?" keyword="Por defecto" description="Si ninguna funcionalidad se adecúa a las necesidades del usuario, será posible utilizar el modo libre para mandar la imagen vista por la cámara a la inteligencia artificial, acompañada por las indicaciones del usuario, permitiendo una interacción completa con la inteligencia artificial."/>
+        <Mode name="Menú" keyword="menú" description="Lee las opciones disponibles."/>
       </div>
 
       <Memoria/>
