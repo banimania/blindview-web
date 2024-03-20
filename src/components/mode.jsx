16056b2bc7128121b1642961d8f10e8398e98f32
banimania <79039680+banimania@users.noreply.github.com>
2024-03-20 08:37:32 +0100

    Memoria y JS

--- src/components/mode.jsx
+++ src/components/mode.jsx
@@ -13,7 +13,7 @@ class Mode extends React.Component {
         <h1 id="mode-name">{this.props.name}</h1>
         <p id="mode-description">{this.props.description}</p>
         <p id="mode-keyword">Palabra clave de activación: <strong>{this.props.keyword}</strong></p>
-        {this.props.example ?   <p id="mode-example">Ejemplo: <strong>{this.props.example}</strong></p> : ""}
+        {this.props.example ? <p id="mode-example">Ejemplo: <strong>{this.props.example}</strong></p> : ""}
       </div>
     )
   }
